59e23914e09973ccfc2ed92263530ddc4ebc7e82
Islam <ibadreldin@ufl.edu>
2016-08-31 00:03:44 -0400

    Wrap all device enumeration functions

--- src/AnalogDiscovery.jl
+++ src/AnalogDiscovery.jl
@@ -4,7 +4,7 @@ const libdwf = Libdl.find_library("libdwf.so")
 
 include("DwfTypes.jl")
 export Hdwf
-for s in instances(EnumFilter)
+for s in instances(ENUMFILTER)
     @eval export $(Symbol(s))
 end
 
--- src/DeviceEnumeration.jl
+++ src/DeviceEnumeration.jl
@@ -1,9 +1,66 @@
 # Device Enumeration API
 
-function enumDevices(enumfilter::EnumFilter = ENUMFILTERALL)
-  x::Int32 = 0
+function enumDevices(enumfilter::ENUMFILTER = ENUMFILTERALL)
+  x = Ref{Cint}(0)
   succ = ccall((:FDwfEnum,libdwf),Cint,(Cint,Ptr{Cint}),
-    enumfilter,pointer_from_objref(x))
-  succ != 1 && error("Error calling FDwfEnum.")
-  return x
+    enumfilter,x)
+  succ == 0 && error("Error calling FDwfEnum.")
+  return x[]
+end
+
+function enumDeviceType(idxDevice::Int32)
+  deviceId = Ref{Cint}(0)
+  deviceRevision = Ref{Cint}(0)
+  succ = ccall((:FDwfEnumDeviceType,libdwf),Cint,(Cint,Ptr{Cint},Ptr{Cint}),
+    idxDevice,deviceId,deviceRevision)
+  succ == 0 && error("Error calling FDwfEnumDeviceType.")
+  return (DEVID(deviceId[]),DEVVER(deviceRevision[]))
+end
+
+function enumDeviceIsOpened(idxDevice::Int32)
+  x = Ref{Cint}(0)
+  succ = ccall((:FDwfEnumDeviceIsOpened,libdwf),Cint,(Cint,Ptr{Cint}),
+    idxDevice,x)
+  succ == 0 && error("Error calling FDwfEnumDeviceIsOpened.")
+  return x[]
+end
+
+function enumUserName(idxDevice::Int32)
+  userName = Vector{UInt8}(32)
+  succ = ccall((:FDwfEnumUserName,libdwf),Cint,(Cint,Ptr{UInt8}),
+    idxDevice,userName)
+  succ == 0 && error("Error calling FDwfEnumDeviceIsOpened.")
+  return String(copy(userName))
+end
+
+function enumDeviceName(idxDevice::Int32)
+  deviceName = Vector{UInt8}(32)
+  succ = ccall((:FDwfEnumDeviceName,libdwf),Cint,(Cint,Ptr{UInt8}),
+    idxDevice,deviceName)
+  succ == 0 && error("Error calling FDwfEnumDeviceIsOpened.")
+  return String(copy(deviceName))
+end
+
+function enumSN(idxDevice::Int32)
+  sn = Vector{UInt8}(32)
+  succ = ccall((:FDwfEnumSN,libdwf),Cint,(Cint,Ptr{UInt8}),
+    idxDevice,sn)
+  succ == 0 && error("Error calling FDwfEnumDeviceIsOpened.")
+  return String(copy(sn))
+end
+
+function enumConfig(idxDevice::Int32)
+  x = Ref{Cint}(0)
+  succ = ccall((:FDwfEnumConfig,libdwf),Cint,(Cint,Ptr{Cint}),
+    idxDevice,x)
+  succ == 0 && error("Error calling FDwfEnumDeviceIsOpened.")
+  return x[]
+end
+
+function enumConfigInfo(idxConfig::Int32,infoType::DWFENUMCONFIGINFO)
+  x = Ref{Cint}(0)
+  succ = ccall((:FDwfEnumConfigInfo,libdwf),Cint,(Cint,Cint,Ptr{Cint}),
+    idxConfig,infoType,x)
+  succ == 0 && error("Error calling FDwfEnumDeviceIsOpened.")
+  return x[]
 end
