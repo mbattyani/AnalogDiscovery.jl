63798c7839b323635a997a888478515be5a63219
Islam Badreldin <ibadreldin@ufl.edu>
2016-08-31 11:11:20 -0400

    Add support for Windows

--- src/AnalogDiscovery.jl
+++ src/AnalogDiscovery.jl
@@ -1,6 +1,12 @@
 module AnalogDiscovery
 
-const libdwf = Libdl.find_library("libdwf.so")
+@static if is_unix()
+  const libdwf = Libdl.find_library("libdwf.so")
+end
+
+@static if is_windows()
+  const libdwf = Libdl.find_library("dwf.dll")
+end
 
 include("DwfTypes.jl")
 export Hdwf
